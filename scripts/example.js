7645e74fe412d93f163a779b72920d9cd5e80c6a
Santtu Keskinen <santtu.keskinen@gmail.com>
2015-08-25 01:32:34 +0300

    Decent demo.

--- scripts/example.js
+++ scripts/example.js
@@ -1,7 +1,27 @@
+var isEnter = e => e.keyCode === 13;
+var Search = React.createClass({
+  componentDidMount: function() {
+    var input = this.refs.search.getDOMNode();
+    var searchEventStream = Bacon.fromEvent(input, "keydown").filter(isEnter)
+      .map(function() {var tmp = input.value; input.value = ''; return tmp});
+    this.props.returnStream.push(searchEventStream);
+    this.props.returnStream.end();
+  },
+
+  render: function() {
+    return (
+      <div className="searchContainer">
+        <h1> Search </h1>
+        <input ref="search" placeholder="Search..."/>
+      </div>
+    );
+  }
+});
+
 var MovieList = React.createClass({
   render: function() {
-    var movieNodes = this.props.data.map(movie => 
-      <Movie data={movie} />
+    var movieNodes = this.props.data.map((movie, i) => 
+      <Movie key={i} data={movie} returnStream={this.props.returnStream}/>
     );
     return (
       <div className="movieList">
@@ -12,6 +32,12 @@ var MovieList = React.createClass({
 });
 
 var Movie = React.createClass({
+  componentDidMount: function() {
+    //var data = this.props.data;
+    var buyButton = this.refs.buy.getDOMNode();
+    var buyEventStream = Bacon.fromEvent(buyButton, "click").map(() => this.props.data.show_title);
+    this.props.returnStream.push(buyEventStream);
+  },
   render: function() {
     var data = this.props.data;
     return (
@@ -23,31 +49,59 @@ var Movie = React.createClass({
         <span className="movieCast"> <b> Cast: </b>{data.show_cast} </span>
         <br/>
         <span className="movieRating"> <b> Rating: </b>{data.rating} </span>
+        <br/>
+        <br/>
+        <div ref="buy" className="btn">Buy</div>
       </div>
     );
   }
 });
 
-var data = Bacon.fromPromise(
-  $.ajax('http://netflixroulette.net/api/api.php?actor=Saori%20Hayami')
-);
+var Library = React.createClass({
+  render: function() {
+    var nodes = this.props.data.map(e => <li> {e} </li>);
+    return (
+      <div className="library">
+        <h1>My library</h1>
+        <ul>
+          {nodes}
+        </ul>
+      </div>
+    );
+  }
+});
 
+var apiUrl = 'http://netflixroulette.net/api/api.php?actor=';
 var App = React.createClass({
   getInitialState: function() {
-    return {data: []};
+    return {movies: [], library: []};
   },
-  loadCommentsFromServer: function() {
-  },
-  componentDidMount: function() {
+  searchStream: new Bacon.Bus(),
+  buyStream: new Bacon.Bus(),
+  componentWillMount: function() {
+    var search = this.searchStream.flatMap(_.identity);
+    var movies = search.flatMapLatest(s => 
+      Bacon.fromPromise($.ajax(apiUrl + s))).toProperty([]);
+    var buyEvents = this.buyStream.flatMap(_.identity);
+    var library = buyEvents.scan([], function(acc,e){
+      if(_.all(acc, a => a !== e)) return acc.concat(e);
+    else return acc;
+    }).toProperty();
+
     var state = Bacon.combineTemplate({
-      data
+      movies,
+      search,
+      library
     }).toEventStream().onValue(this.setState.bind(this));
+
   },
   render: function() {
     return (
       <div className="app">
-        <h1>Hayami Saori movies</h1>
-        <MovieList data={this.state.data} />
+        <Library data={this.state.library} />
+        <Search returnStream={this.searchStream} />
+        <h1>{this.state.search} movies</h1>
+        <MovieList data={this.state.movies} returnStream={this.buyStream} />
       </div>
     );
   }
